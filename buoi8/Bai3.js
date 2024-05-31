ab2a7d1f351253decbed48456c5fb78c6f42b461
nguyentruong032 <thesucceeder032045@gmail.com>
2024-05-31 21:04:19 +0700

    sua

--- buoi8/Bai3.js
+++ buoi8/Bai3.js
@@ -1,21 +1,21 @@
 var x = Math.round(Math.random()); // Tạo ra một số bất kì 0 hoặc 1
-var KT = prompt("Nhâp một trong hai số bất kì là 0 và 1 ");
-document.write("chon 1 hoac 0 <br><br>");
+var KT = prompt("Chọn  một trong hai số bất kì là 0 và 1 ");
+document.write("CHỌN 1 HOẶC 0  <br><br>");
 if (KT == x) {
-  document.write("Thầy Huấn <br><br> ");
+  document.write("BẠN ĐÃ CHỌN ĐÚNG  <br><br> ");
 } else {
-  document.write("Em Tú Senna  <br><br> ");
+  document.write("BẠN ĐÃ CHỌN SAI  <br><br> ");
 }
 
-document.write("chon bat ki 1-10 <br><br>");
+document.write("CHỌN 1 SỐ TỪ 1 ĐẾN 10  <br><br>");
 
 //  Tạo biến x có giá trị ngẫu nhiên từ 0 đến 10
 var Y = Math.floor(Math.random() * 11);
-var KQ = prompt("Nhâp một trong hai số bất kì từ 0 đến 10 ");
+var KQ = prompt("Nhâp một số bất kì từ 0 đến 10 ");
 
 // In giá trị của x ra màn hình
 if (KQ == Y) {
-  document.write("  Bốc Bát Họ Mà Làm Thêm Ván Nữa ");
+  document.write("  BẠN ĐÃ NHẬP ĐÚNG  ");
 } else {
-  document.write(" Về Đi Đừng Nghiện ");
+  document.write(" BẠN ĐÃ NHẬP SAI ");
 }
